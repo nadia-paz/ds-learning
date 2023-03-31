69115e6c20a7d34f8a4eda8ad0ddb1028a258687
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-03-30 19:47:51 -0500

    start ann

---
+++ dl/ann.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
