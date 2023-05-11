83c1d4b89e01b203c7262bb50a9ed571a6ba2310
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-05-10 20:04:39 -0500

    add func

--- python/data_structures/move_boxes.py
+++ python/data_structures/move_boxes.py
@@ -38,3 +38,56 @@ for key, value in obstacles.items():
     print(key)
     print(value)
 
+def move_boxes(matrix):
+    # Get the dimensions of the matrix
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # Find the positions of all the boxes
+    boxes = []
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == '#':
+                boxes.append((i, j))
+
+    # Move the boxes to the right
+    for box in boxes:
+        i, j = box
+        while j < cols - 1 and matrix[i][j+1] == '.':
+            matrix[i][j] = '.'
+            matrix[i][j+1] = '#'
+            j += 1
+
+    # Find the position of boxes again
+    boxes = []
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == '#':
+                boxes.append((i, j))
+            
+            
+    # Move the boxes down
+    for box in boxes:
+        i, j = box
+        while i < rows - 1 and matrix[i+1][j] == '.':
+            matrix[i][j] = '.'
+            matrix[i+1][j] = '#'
+            i += 1
+
+    return matrix
+
+m = [
+    ['.', '#', '*', '#', '.'],
+    ['.', '.', '.', '.', '.'],
+    ['.', '.', '.', '#', '*'],
+    ['#', '#', '.', '.', '.'],
+    ['#', '#', '.', '.', '.'],
+]
+rows1 = len(m)
+for row in range(rows1):
+    print(m[row])
+print()
+m1 = move_boxes(m) 
+rows = len(m1)
+for row in range(rows):
+    print(m1[row])
