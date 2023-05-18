43e17a68d1092baf84fd59835248ef359d4ab772
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-05-18 17:22:16 -0500

    load data

--- sf/tables_setup.sql
+++ sf/tables_setup.sql
@@ -7,12 +7,12 @@ CREATE OR REPLACE SCHEMA MOVIE_SCHEMA;
 -- create file format
 CREATE or REPLACE file format csvformat
   skip_header = 0
-  type = 'CSV';
+  type = 'CSV'
+  FIELD_DELIMITER = '\t';
 
   -- create stage
--- CREATE or REPLACE stage ratings_data_stage
---   file_format = csvformat
---   url = '/Users/nadina/Documents/ml-100k/ratings.csv';
+CREATE or REPLACE stage ratings_data_stage
+  file_format = csvformat;
 
 
 -- create a table where we upload data
@@ -21,13 +21,51 @@ CREATE or REPLACE TABLE MOVIE_DB.MOVIE_SCHEMA.ratings (
     user_id NUMBER(38,0),
     item_id NUMBER(38,0),
     rating FLOAT,
-    timestamp DATE
+    timestamp number(38,0)
 );
-STAGE_FILE_FORMAT = ( TYPE = 'csv' FIELD_DELIMITER= '\t' );
 
-put file://'/Users/nadina/Documents/ml-100k/ratings.csv' ratings
+-- !!!use SNOWSQL in the terminal to upload data!!!
+-- put file:///Users/nadina/Documents/ml-100k/ratings.csv @ratings_data_stage;
+-- nadina#COMPUTE_WH@MOVIE_DB.MOVIE_SCHEMA>put file:///Users/nadina/Documents/ml-100k/ratings.csv @ratings_data_stage;
+-- +-------------+----------------+-------------+-------------+--------------------+--------------------+----------+---------+
+-- | source      | target         | source_size | target_size | source_compression | target_compression | status   | message |
+-- |-------------+----------------+-------------+-------------+--------------------+--------------------+----------+---------|
+-- | ratings.csv | ratings.csv.gz |     1979173 |      828256 | NONE               | GZIP               | UPLOADED |         |
+-- +-------------+----------------+-------------+-------------+--------------------+--------------------+----------+---------+
 
--- copy from file into the table
+-- copy from the stage into the table
 COPY into ratings
   from @ratings_data_stage;
 
+-- Check if it worked
+select *
+from ratings;
+-- success!!!
+
+-- will need later foreign keys (user_id and item_id)
+
+
+-- CREATE A TABLE items (contains movies info)
+-- STEPS:
+-- 1. File format
+-- 2. Stage
+-- 3. Table
+-- 4. snowsql put file into the stage
+-- 5. COPY from the stage into the table
+
+CREATE IF NOT EXISTS file format csvpipe
+  skip_header = 0
+  type = 'CSV'
+  FIELD_DELIMITER = '|';
+
+CREATE OR REPLACE stage items_data_stage
+    file_format = csvpipe;
+
+CREATE OR REPLACE table MOVIE_DB.MOVIE_SCHEMA.items
+-- movie id | movie title | release date | video release date |
+-- IMDb URL | unknown | Action | Adventure | Animation |
+-- Children's | Comedy | Crime | Documentary | Drama | Fantasy |
+-- Film-Noir | Horror | Musical | Mystery | Romance | Sci-Fi |
+-- Thriller | War | Western |
+item_id NUMBER(38, 0),
+title 
