c487c1d234a1f2ee940d35d01e9df4fe11be23eb
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-06-08 19:01:56 -0500

    tuple exercise

---
+++ python/exercises/tuples.py
@@ -0,0 +1,13 @@
+'''
+Given an integer, n , and n space-separated integers as input, create a tuple, t, of those n integers. 
+Then compute and print the result of hash(t).
+
+'''
+
+if __name__ == 'main':
+    n = int(input())
+    s = input()
+    l = map(int, s.split())
+    t = tuple(l[:n])
+    print(hash(t))
+    # code is not accepted by hacker rank
