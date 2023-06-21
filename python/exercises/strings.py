a88438ad5b87313e6a07a23153f0c368d2794f3b
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-06-21 18:23:15 -0500

    add logo exercise

--- python/exercises/strings.py
+++ python/exercises/strings.py
@@ -54,3 +54,34 @@ def print_string_properties(s:str) -> None:
     print(any([char.isdigit() for char in s]))
     print(any([char.islower() for char in s]))
     print(any([char.isupper() for char in s]))
+
+# Logo print
+#Replace all ______ with rjust, ljust or center. 
+
+# thickness = int(input()) #This must be an odd number
+# c = 'H'
+def print_logo(c: str='H', thickness: int=6):
+    #Top Cone
+    for i in range(thickness):
+        print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+    #Top Pillars
+    for i in range(thickness+1):
+        print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+    #Middle Belt
+    for i in range((thickness+1)//2):
+        print((c*thickness*5).center(thickness*6))    
+
+    #Bottom Pillars
+    for i in range(thickness+1):
+        print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+    #Bottom Cone
+    for i in range(thickness):
+        print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+def wrap(string, max_width):
+
+    return textwrap.fill(string, max_width)
+
