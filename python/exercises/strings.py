fce7601ba44484e586fa030e98d5d534df56506b
Nadia Paz <nadina.abouhamdan@gmail.com>
2023-06-26 15:16:20 -0500

    add roman numbers

--- python/exercises/strings.py
+++ python/exercises/strings.py
@@ -158,3 +158,9 @@ def merge_the_tools(string, k):
         s = list(dict.fromkeys(s))
         #substr.append(s)
         print(''.join(s))
+
+# Roman numbers
+regex_pattern = r'^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$'
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
