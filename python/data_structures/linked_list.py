2bcc896401f5528c4de0b8d4df8bc586af2c3873
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-05-15 14:33:00 -0500

    add comments

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,4 +1,7 @@
 class Node:
+    """
+    Creates a Node of a LinkedList.
+    """
     def __init__(self, value):
         self.value = value
         self.next = None
@@ -6,59 +9,104 @@ class Node:
 
 class LinkedList:
     def __init__(self, value):
+        """  
+        Constractor inizializes a LinkedList by creating a new Node with a value
+        and pointing head and tail to this Node. 
+        """
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
+        # keep track of the LL length
         self.length = 1
 
     def print_list(self):
+        """
+        Prints all values of the LinkedList 
+        """
+        # start at head
         temp = self.head
         while temp is not None:
+            # print value of Nodes until reaches tail
             print(temp.value)
+            # move to the next Node
             temp = temp.next
         
     def append(self, value):
+        """ 
+        Appends a new Node to the end of the LinkedList
+        """
+        # create a new node with the value
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
         else:
+            # point to the new node
             self.tail.next = new_node
+            # move tail to the new node
             self.tail = new_node
         self.length += 1
         return True
 
     def pop(self):
+        """
+        Removes the last node from the LinkedList and returns it.
+        Set the tail to the new last node.
+        """
         if self.length == 0:
             return None
+        # create temp (to remove and return)
+        # create pre to be a new tail
+        # point both variables to the head of LL
         temp = self.head
         pre = self.head
+        # while the node is not tail
         while(temp.next):
+            # point pre to temp
             pre = temp
+            # move temp to the next node
             temp = temp.next
+        # when temp hits tail, old tail node is equal temp
+        # set pre as a new tail
         self.tail = pre
         self.tail.next = None
+        # decrease the length of the LL
         self.length -= 1
+        # if new length is 0
         if self.length == 0:
+            # remove pointers
             self.head = None
             self.tail = None
         return temp
 
     def prepend(self, value):
+        """
+        Inserts a new value to the beginning of the LinkedList
+        """
+        # create a new node with value
         new_node = Node(value)
+        # if the LL is empty, assign head and tail pointers to the new node
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
         else:
+            # point the new node to the LL's head
             new_node.next = self.head
+            # move the LL's head to the new node
             self.head = new_node
         self.length += 1
         return True
 
     def pop_first(self):
+        """
+        Removes the first node from the LinkedList. Assigns the head pointer to the 2nd node.
+        Retures removed node
+        """
         if self.length == 0:
             return None
+        # create a temp node
         temp = self.head
+        # move head to the 2nd node
         self.head = self.head.next
         temp.next = None
         self.length -= 1 
