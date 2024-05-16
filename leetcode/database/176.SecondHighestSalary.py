4bbe4ff07a6ce71c6a1b61ba11f01df8b5c4ff55
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-05-16 14:52:14 -0500

    add correct solution

--- leetcode/database/176.SecondHighestSalary.py
+++ leetcode/database/176.SecondHighestSalary.py
@@ -3,12 +3,25 @@ import pandas as pd
 # Write a solution to find the second highest salary from the Employee table. 
 # If there is no second highest salary, return null (return None in Pandas).
 
+# passes the run but not submit
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     if len(employee) < 2:
         return pd.DataFrame([{'SecondHighestSalary':None}])
     else:
         return pd.DataFrame(
             employee['salary'].sort_values().tail(2).head(1)
+            ).rename(
+                columns={'salary':'SecondHighestSalary'}
+                )
+
+
+# correct one, submitted, accepted
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    if len(employee['salary'].drop_duplicates()) < 2:
+        return pd.DataFrame([{'SecondHighestSalary':None}])
+    else:
+        return pd.DataFrame(
+            employee['salary'].drop_duplicates().sort_values().tail(2).head(1)
             ).rename(
                 columns={'salary':'SecondHighestSalary'}
                 )
