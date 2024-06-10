54162cb6c5feef684def25c74b4485feded1c1a2
Nadia Paz <nadina.abouhamdan@gmail.com>
2024-06-10 15:15:15 -0500

    insert value

--- python/data_structures/doubly_linked_list.py
+++ python/data_structures/doubly_linked_list.py
@@ -98,6 +98,21 @@ class DoublyLinkedList:
             temp.value = value
             return True
         return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.prev = temp
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1 
+        return True
 
 dll = DoublyLinkedList(1)
 dll.append(2)
